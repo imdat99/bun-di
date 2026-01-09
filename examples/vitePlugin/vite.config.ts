ea960c23a67f174d607dd68514db7537e3a5f606
lethdat <imdat2999@gmail.com>
2026-01-10 00:24:18 +0700

    refactor: comment out unused plugin imports in vite.config.ts

--- examples/vitePlugin/vite.config.ts
+++ examples/vitePlugin/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import honoDiVite from '../../packages/vite/dist'
+// import honoDiVite from '../../packages/vite/dist'
 // import honoDi from '../../packages/vite/src/index'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [honoDiVite()],
+  // plugins: [honoDiVite()],
   server: {
     port: 3001,
   },
